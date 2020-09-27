678fffafbcc6183078d501d52e18e1fb0efb1d97
JordanField <j.field.2@warwick.ac.uk>
2020-09-27 15:49:52 +0100

    build 202009271600

---
+++ precache-manifest.facbf33c754a1518b6b7465ba7dcdb6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5decbd7b29ce2ae06d41ad272216b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7846c4628aec85fa41f9",
+    "url": "/static/css/main.61d541bf.chunk.css"
+  },
+  {
+    "revision": "a96bfaafde9abbfb37f2",
+    "url": "/static/js/2.2bec17d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bec17d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7846c4628aec85fa41f9",
+    "url": "/static/js/main.1e9dfcaf.chunk.js"
+  },
+  {
+    "revision": "fcf9cda353d5bb39cd40",
+    "url": "/static/js/runtime-main.0684dc96.js"
+  }
+]);
