6668c81f44c7d2c4dff36c25719e827be22a76ad
uow-covid19 <buildbot@example.com>
2020-09-29 11:27:39 +0000

    Update from https://github.com/uow-covid19/uow-covid19.github.io.dev/commit/68ac1ba1d30cfb54a1623f3c5814cb19eee5be97

---
+++ liverpool/precache-manifest.2f7a15690cc1540fa9ccd9060f2610be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a56671be58ea8ba94e83f3000bfc39e",
+    "url": "/liverpool/index.html"
+  },
+  {
+    "revision": "6a87f03b3ab89d342a75",
+    "url": "/liverpool/static/css/main.cbe442cf.chunk.css"
+  },
+  {
+    "revision": "2b85b4d0aed03d98b8e9",
+    "url": "/liverpool/static/js/2.c623ae1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liverpool/static/js/2.c623ae1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a87f03b3ab89d342a75",
+    "url": "/liverpool/static/js/main.a52659ce.chunk.js"
+  },
+  {
+    "revision": "665b80ef4a133f730551",
+    "url": "/liverpool/static/js/runtime-main.fc8e2fe1.js"
+  }
+]);
