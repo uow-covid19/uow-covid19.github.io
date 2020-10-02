a67bec3ad2c154a0ac5d70836319904c7ddc5ea2
uow-covid19 <buildbot@example.com>
2020-10-02 17:01:35 +0000

    Update from https://github.com/uow-covid19/uow-covid19.github.io.dev/commit/4997d1dbc58290ed20fa2c80affbf4aa50c048a4

---
+++ warwick/precache-manifest.30ac78b97416496456e9a075a65ad7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a75a82cfccb4489a49886aef68fb96",
+    "url": "/warwick/index.html"
+  },
+  {
+    "revision": "8d2661464b864cea7d88",
+    "url": "/warwick/static/css/main.9fc48b67.chunk.css"
+  },
+  {
+    "revision": "3400fb3849461c4bc93e",
+    "url": "/warwick/static/js/2.83d83e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/warwick/static/js/2.83d83e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2661464b864cea7d88",
+    "url": "/warwick/static/js/main.41488fe3.chunk.js"
+  },
+  {
+    "revision": "075a3d92d6b989019562",
+    "url": "/warwick/static/js/runtime-main.1432b9be.js"
+  }
+]);
