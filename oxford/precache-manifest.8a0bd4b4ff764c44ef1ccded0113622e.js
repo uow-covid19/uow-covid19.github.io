e630f06f696c009a500ed7aa7e06ec3433d073c8
uow-covid19 <buildbot@example.com>
2020-10-02 16:49:29 +0000

    Update from https://github.com/uow-covid19/uow-covid19.github.io.dev/commit/25b70f462899453bf1f51f650430d8da5d91a5ce

---
+++ oxford/precache-manifest.8a0bd4b4ff764c44ef1ccded0113622e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e77173a62ff90577e68a252282937c",
+    "url": "/oxford/index.html"
+  },
+  {
+    "revision": "6e44fb1d18bb28976dfa",
+    "url": "/oxford/static/css/main.9fc48b67.chunk.css"
+  },
+  {
+    "revision": "3400fb3849461c4bc93e",
+    "url": "/oxford/static/js/2.83d83e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oxford/static/js/2.83d83e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e44fb1d18bb28976dfa",
+    "url": "/oxford/static/js/main.73d36925.chunk.js"
+  },
+  {
+    "revision": "57c5a18e7b3fba440fb3",
+    "url": "/oxford/static/js/runtime-main.094e0e1b.js"
+  }
+]);
